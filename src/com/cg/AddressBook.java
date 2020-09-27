48d503bd554cfcec620a53c73f2ca5f60d534bc0
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-09-28 00:25:25 +0530

    [Arijit]Refactored : Multiple Address Books are Available

--- src/com/cg/AddressBook.java
+++ src/com/cg/AddressBook.java
@@ -4,4 +4,5 @@ public interface AddressBook {
   public void createContact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNo, String email);
   public void editContact(String firstName, String lastName);
   public void deleteContact(String firstName, String lastName);
+  public void showDetails();
 }
